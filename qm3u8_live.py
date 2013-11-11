25ad5e7d0abecd5bf10a18d011af47f2688cfcd8
dtynn <dtynn@163.com>
2013-11-11 14:36:37 +0800

    add

--- qm3u8_live.py
+++ qm3u8_live.py
@@ -2,13 +2,13 @@
 from optparse import OptionParser
 
 
-def livePlaylist(content, prefix=None, listSize=3, isLive=True, allowCache=False):
+def livePlaylist(content, prefix=None, listSize=3, allowCache=False):
     if not content:
         return ''
     contentList = content.split('\n')
     cacheTagCt = 0
     cacheTag = '#EXT-X-ALLOW-CACHE:YES' \
-        if (isLive is not True) or (allowCache is True) \
+        if (allowCache is True) \
         else '#EXT-X-ALLOW-CACHE:NO'
     hasSegSec = False
     inSegSec = False
@@ -31,7 +31,12 @@ def livePlaylist(content, prefix=None, listSize=3, isLive=True, allowCache=False
 
     if hasSegSec is True:
         headSec = contentList[:segSecStart]
-        seqSec = contentList[segSecStart:][-3:] if inSegSec else contentList[segSecStart:segSecEnd][-3:]
+        start = - listSize * 2
+        seqSec = contentList[segSecStart:][start:] if inSegSec else contentList[segSecStart:segSecEnd][start:]
+        if prefix:
+            for ln, line in enumerate(seqSec):
+                if line.startswith('#') is not True:
+                    seqSec[ln] = '%s%s' % (prefix, line)
         tailSec = [] if inSegSec else contentList[segSecEnd:]
         return '%s\n%s\n%s' % ('\n'.join(headSec), '\n'.join(seqSec), '\n'.join(tailSec))
     else:
@@ -41,23 +46,41 @@ def livePlaylist(content, prefix=None, listSize=3, isLive=True, allowCache=False
 def optParser():
     optp = OptionParser()
     #settings
-    optp.add_option('-u', '--url', help='url of the m3u8 file',
-                    dest='url', default=None)
+    optp.add_option('-p', '--prefix', help='prefix',
+                    dest='prefix', default=None)
     optp.add_option('-o', '--output', help='output file path',
                     dest='output', default=None)
-    optp.add_option('-e', '--expires', help='expire time',
-                    dest='expires', default=3600)
+    optp.add_option('-i', '--input', help='input file path',
+                    dest='input', default=None)
+    optp.add_option('-l', '--listsize', help='m3u8 list size',
+                    dest='listsize', default=3)
+    optp.add_option('-c', '--cache', help='allow cache',
+                    dest='cache', default=None)
 
     opts, args = optp.parse_args()
-    return opts.url, opts.output, int(opts.expires)
+    return opts.prefix, opts.input, opts.output, int(opts.listsize), bool(opts.cache)
+
+
+def listOutput(fPath, content):
+    with file(fPath, 'w') as f:
+        f.write(content)
+    return
 
 
 def main():
-    url, output, expires = optParser()
-    if not url:
-        printUsage()
+    prefix, fInput, fOutput, listSize, allowCache = optParser()
+
+    if not fInput:
+        print 'need a file'
         return
-    print getPrivateM3U8(url, output, expires)
+
+    with file(fInput, 'r') as f:
+        content = f.read()
+        result = livePlaylist(content, prefix, listSize, allowCache)
+        print result
+        if fOutput:
+            listOutput(fOutput, result)
+        return
 
 
 if __name__ == '__main__':
